e513e3628301f585ba39fcee23e25bd44a4a13e7
Vihang Patil <Vihangpatil37@gmail.com>
2026-09-07 22:08:27 +0530

    new comic form page

---
+++ frontend/app/admin/comics/new/page.tsx
@@ -0,0 +1,10 @@
+import ComicForm from "../../../../components/ComicForm";
+
+export default function NewComicPage() {
+  return (
+    <div>
+      <h2 className="text-xl mb-6">Upload New Comic</h2>
+      <ComicForm />
+    </div>
+  );
+}
